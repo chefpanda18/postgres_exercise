3d976cfd81ffe79c8fab1cab8c35929d8f25873d
Amanda <75429795+chefpanda18@users.noreply.github.com>
2021-04-17 20:57:58 -0400

    Add files via upload

---
+++ queries_products.sql
@@ -0,0 +1,28 @@
+-- Comments in SQL Start with dash-dash --
+
+-- INSERT INTO products (name, price, can_be_returned) VALUES ('chair', '44', 'false');
+
+-- INSERT INTO products (name, price, can_be_returned) VALUES ('stool', '25.99', 'true');
+
+-- INSERT INTO products (name, price, can_be_returned) VALUES ('table', '124', 'false');
+
+-- SELECT * FROM products;
+
+-- SELECT name FROM products;
+
+-- SELECT name, price FROM products;
+
+-- INSERT INTO products (name, price, can_be_returned) VALUES ('table and benches set', '300', 'true');
+
+-- SELECT name FROM products WHERE can_be_returned = true;
+
+-- SELECT price FROM products WHERE price < 44;
+
+-- SELECT name FROM products WHERE price BETWEEN 22.50 AND 99.99;
+
+-- UPDATE products SET price = price -20;
+
+-- DELETE FROM products WHERE price < 25;
+
+-- UPDATE products SET price = price +20;
+
